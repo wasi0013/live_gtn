4b1dd2def64fb82afd8288c2dffa4289197e3a69
Wasi Mohammed Abdullah <wasi0013@gmail.com>
2021-06-18 12:26:26 +0600

    initial version of gtn

--- lib/live_gtn_web/live/page_live.ex
+++ lib/live_gtn_web/live/page_live.ex
@@ -3,17 +3,48 @@ defmodule LiveGtnWeb.PageLive do
 
   @impl true
   def mount(_params, _session, socket) do
-    {:ok, assign(socket, score: 0, number:  Enum.random(1000..9999))}
+    {:ok, assign(socket,
+        score: 1000,
+        number:  "????",
+        answer: "#{Enum.random(1000..9999)}",
+        message: "Make a Guess!"
+        )}
   end
 
   @impl true
-  def handle_event("guess", %{"ans" => ans}, socket) do
-    {:noreply, assign(socket, results: 0, ans: ans)}
+  def handle_event("guess", %{"number" => number}, socket) when number == socket.assigns.answer do
+    {:noreply, assign(socket,
+         number: number,
+         message: "You Won!"
+         )}
+  end
+
+  @impl true
+  def handle_event("guess", %{"number" => number}, socket) do
+    score = socket.assigns.score - 1
+    if (Integer.parse(socket.assigns.answer) < Integer.parse(number)) do
+      {:noreply, assign(socket,
+      message: "Your guess is too high. Try again!",
+      score: score
+      )}
+    else
+      {:noreply, assign(socket,
+      message: "Your guess is too Low. Try again!",
+      score: score
+      )}
+    end
+
+
   end
 
   @impl true
   def handle_event("reset", %{"value" => ""}, socket) do
-    {:noreply, assign(socket, score: 13, number: Enum.random(1000..9999))}
+    {:noreply, assign(socket,
+        score: 0,
+        number: "????",
+        answer: "#{Enum.random(1000..9999)}",
+        message: "Make a Guess!"
+        )}
   end
 
 end
